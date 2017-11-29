5d9f091e59050b6a24f43164ada0928abcab725e
BrainFooLong <roland@bfldev.com>
2017-11-29 16:29:31 +0100

    updated release zip script

--- dev/create-release.php
+++ dev/create-release.php
@@ -5,13 +5,9 @@ namespace Nullix\Sasige;
 require __DIR__ . "/../sasige/src/_init.php";
 
 $ignoreFiles = [
-    "^/\.git(/|$)",
-    "^/\.idea(/|$)",
-    "^/dev(/|$)",
-    "^/public/.+?",
-    "^/static/.+?"
+
 ];
-$sourceDir = File::concat(File::sanitizePath(dirname(__DIR__)));
+$sourceDir = File::concat(File::sanitizePath(dirname(__DIR__))."/sasige");
 $destDir = File::concat(File::sanitizePath(__DIR__), "tmp");
 $files = File::getFiles($sourceDir, null, true, true);
 $releaseFile = "release-" . SASIGE_VERSION . ".zip";
