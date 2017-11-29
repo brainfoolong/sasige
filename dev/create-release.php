919c1b743782f49c55c66c7c66d81aec81ceaf50
BrainFooLong <brainfoolong@users.noreply.github.com>
2017-11-29 15:57:24 +0100

    upgraded to 0.3 copyFiles now can also handle directories added more tests

--- dev/create-release.php
+++ dev/create-release.php
@@ -4,15 +4,27 @@ namespace Nullix\Sasige;
 
 require __DIR__ . "/../sasige/src/_init.php";
 
-$sourceDir = File::concat(File::sanitizePath(dirname(__DIR__)), "sasige");
+$ignoreFiles = [
+    "^/\.git(/|$)",
+    "^/\.idea(/|$)",
+    "^/dev(/|$)",
+    "^/public/.+?",
+    "^/static/.+?"
+];
+$sourceDir = File::concat(File::sanitizePath(dirname(__DIR__)));
 $destDir = File::concat(File::sanitizePath(__DIR__), "tmp");
-$files = File::getFiles($sourceDir, null, true);
+$files = File::getFiles($sourceDir, null, true, true);
 $releaseFile = "release-" . SASIGE_VERSION . ".zip";
 
 $copyFiles = [];
 
 foreach ($files as $file) {
     $relativePath = str_replace($sourceDir, "", $file);
+    foreach ($ignoreFiles as $regex) {
+        if (preg_match("~$regex~i", $relativePath)) {
+            continue 2;
+        }
+    }
     $dest = $destDir . $relativePath;
     $copyFiles[$file] = $dest;
 }
--- tests/Common/MiscTest.php
+++ tests/Common/MiscTest.php
@@ -2,6 +2,7 @@
 
 namespace Nullix\Tests\Common;
 
+use Nullix\Sasige\Config;
 use Nullix\Sasige\File;
 use PHPUnit\Framework\TestCase;
 
@@ -11,5 +12,20 @@ final class MiscTest extends TestCase
     {
         $this->assertEquals([], File::getFiles("FOO"));
         File::deleteDirectory(SASIGE_PROJECT_ROOT . "/public");
+
+        Config::setStaticFolder("static");
+        $this->assertEquals("static", Config::getStaticFolder());
+
+        Config::setThemeFolder("theme");
+        $this->assertEquals("theme", Config::getThemeFolder());
+
+        Config::setOutputFolder("public");
+        $this->assertEquals("public", Config::getOutputFolder());
+
+        Config::setPagesFolder("pages");
+        $this->assertEquals("pages", Config::getPagesFolder());
+
+        Config::setServerPort(4433);
+        $this->assertEquals(4433, Config::getServerPort());
     }
 }
